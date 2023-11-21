f296237902b7671c81f4dd8199860037c0006b6e
Luigi Perkoski <celluigipplc@gmail.com>
2023-11-20 22:09:25 -0300

    Create models and new template

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Room(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    message = models.ManyToManyField('Message')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.index),
 ]
