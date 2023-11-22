6447d1ba5c3ddcb324f36cb94cd324ce22041700
Luigi Perkoski <celluigipplc@gmail.com>
2023-11-21 23:57:48 -0300

    iniciando front-end

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Room, Message
 
 def index(request):
 
-    context = {}
+    mensage = Message.objects.filter(user=request.user.id)
+
+    context = {'msg': mensage}
 
     return render(request, 'pages/index.html', context)
