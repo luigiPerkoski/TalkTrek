1c6e938850ff88f23f7545d25239a285e7912a94
LuigiPerkoski <celluigipplc@gmail.com>
2024-07-19 23:31:04 -0300

    create models User, Message and Room

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    friends = models.ManyToManyField('self', blank=True, related_name='friends')
+    bio = models.TextField(blank=True, null=True)
+    online_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f' {self.username} || {self.email}'
+
+
+class Message(models.Model):
+    STATUS_CHOICES = [
+        ('sent', 'Sent'),
+        ('received', 'Received'),
+        ('read', 'Read'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    texto = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='sent')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.texto[:50]} - {self.status}'
+
+
+class Room(models.Model):
+    friends = models.ManyToManyField(User, related_name='rooms')
+    user = models.ForeignKey(User, related_name='created_rooms', on_delete=models.CASCADE)
+    message_list = models.ManyToManyField(Message, blank=True)
+
+
