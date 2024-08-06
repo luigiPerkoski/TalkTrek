ec92d40a51ab04135d444d14f57a1c7e4da683a0
LuigiPerkoski <celluigipplc@gmail.com>
2024-08-05 23:07:00 -0300

    Updated user url to accept optional parameter

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('user', views.GetUsers, name='get_all_users'),
-    path('user/<int:id>', views.GetUsersById, name='get_users_by_id'),
+    re_path(r'^user/(?P<id>\d+)?/?$', views.UsersView, name='users'),
 ]
+
