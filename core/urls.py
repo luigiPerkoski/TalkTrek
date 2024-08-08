bfb3731cefd445d5887c7da94039a665e338ea0a
LuigiPerkoski <celluigipplc@gmail.com>
2024-08-07 22:31:51 -0300

    updating a small detail

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.users, name='users'),
-    path('user/<id>', views.user_by_id, name='user_by_id'),
+    path('user/', views.user_manager, name='users'),
 ]
 
